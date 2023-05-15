bfbe11d16dafcd14c59136d94e458f4f84d0816a
isoboev <99904482+isoboev@users.noreply.github.com>
2023-05-15 04:10:34 +0300

    Add files via upload

---
+++ cursor/js/app.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.cursor').forEach(node => {
+  document.addEventListener('mousemove', e => {
+    document.body.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px;`
+  })
+
+})
